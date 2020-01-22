d9a24cf9a7625cc7949e87ba244be0e1ced8c1ad
Sofia <ruthsofia95@hotmail.com>
2020-01-22 10:25:32 -0600

    Base classes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int score;
+    public static GameManager instance;
+
+    int[,] board;
+
+    public LevelData levelData;
+
+    private enum GridInfo
+    {
+        Obstacle, //cannot detect input here
+        Playable //contains a tile
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        board = new int[levelData.gridSize.x, levelData.gridSize.y];
+    }
+
+    private void CreateBoard()
+    {
+        for(int row = 0; row < levelData.gridSize.x; row++)
+        {
+
+            for (int col = 0; col < levelData.gridSize.y; col++)
+            {
+                //for now, it will be a square grid, 
+                //TODO figure out how to make the levels uneven
+                board[row, col] = (int)GridInfo.Playable;
+            }
+        }
+    }
+
+    #region SCORE
+    /// <summary>
+    /// Returns the scores
+    /// </summary>
+    /// <returns></returns>
+    public int GetScore()
+    {
+        return score;
+    }
+
+    /// <summary>
+    /// Adds this amount to the score. +=
+    /// </summary>
+    /// <param name="valueToAdd"></param>
+    public void AddScore(int valueToAdd)
+    {
+        score += valueToAdd;
+    }
+    #endregion
+
+}
---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelData", 
+    menuName = "ScriptableObjects/Level Information", order = 1)]
+public class LevelData : ScriptableObject
+{
+    public Vector2Int gridSize = new Vector2Int(5, 5);
+}
